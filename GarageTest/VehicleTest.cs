49dd669451b389fb9964ab209f8b97de79d546e9
rosy871 <yalameju@gmail.com>
2021-07-09 09:47:07 +0200

    added mstest _ version problem

---
+++ GarageTest/VehicleTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageTest
+{
+    [TestClass]
+    public class VehicleTest
+    {
+        [TestMethod]
+        public void Stats_CreatObj_PrintStats()
+        {
+            string name = "audi";
+            string regnum = "abc123";
+            string color = "red";
+            int wheelnum = 4;
+            var vechile = new Garage1._0.Vehicle(name, regnum, color, wheelnum);
+            string actual = vechile.Stats();
+            string expected = $"Name : {name,-13} Register Number : {regnum,-10} color : {color,-10} No. of Wheels : {wheelnum,-5}  ";
+            Assert.AreEqual(actual, expected);
+
+        }
+    }
+}
